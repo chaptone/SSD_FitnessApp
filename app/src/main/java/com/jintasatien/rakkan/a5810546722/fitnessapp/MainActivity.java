d368ab5d2b503782c3f92fa7798e263a1e108bf1
Rakkan Jintasatien <rakkan.j@ku.th>
2017-06-02 19:15:48 +0700

    init

---
+++ app/src/main/java/com/jintasatien/rakkan/a5810546722/fitnessapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jintasatien.rakkan.a5810546722.fitnessapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
